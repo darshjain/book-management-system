1cfeec06afa29e0d420641508fd152e0383c845f
darshjain <darshj786@gmail.com>
2021-03-23 12:33:06 +0530

    minor background fix

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
 from database import DB as dbh
-
+from PIL import ImageTk, Image
 db = dbh()
-selected_tuple=[]
+selected_tuple = []
+
 
 def get_selected_row(event):
     global selected_tuple
@@ -50,9 +51,16 @@ def update_command():
 window = Tk()
 
 window.title("My Books")
-bg = PhotoImage(file = "lib3.png")
-window.configure(background="cornsilk2") #background color
-window.geometry("600x600") #window size
+# ================== CHANGES HERE START========================
+canv = Canvas(window, width=700, height=700, bg='white')
+canv.place(relx=0, rely=0, anchor=NW)
+img = Image.open("lib3.png")
+img = img.resize((700, 700), Image.ANTIALIAS)  # PIL solution
+img_save = ImageTk.PhotoImage(img)
+canv.create_image(0, 0, anchor=NW, image=img_save)
+# ================== CHANGES HERE END ========================
+window.geometry("700x700")  # window size
+
 
 def on_closing():
     dd = db
@@ -63,21 +71,17 @@ def on_closing():
 
 window.protocol("WM_DELETE_WINDOW", on_closing)
 
-lb1 = Label(window,text="  ", font=("Courier", 16, "italic"),bg= "Cornsilk2", fg="Black")
-lb2 = Label(window,text="Book Management System", font=("Courier", 20, "italic"),bg= "Cornsilk2", fg="Black")
-lb3 = Label(window,text="", font=("Courier", 16, "italic"),bg= "Cornsilk2", fg="Black")
-lb4 = Label(window,text="", font=("Courier", 16, "italic"),bg= "Cornsilk2", fg="Black")
-#heading
+lb2 = Label(window, text="Book Management System",
+            font=("Courier", 20, "italic"), fg="Black")
 
-lbl_title = Label(window, text="Title" ,bg = "cornsilk2")
-lbl_author = Label(window, text="Author", bg="cornsilk2")
-lbl_isbn = Label(window, text="ISBN", bg="cornsilk2")
 
-lb5 = Label(window,text="", font=("Courier", 15, "italic"),bg= "Cornsilk2", fg="Black")
-#Space
+lbl_title = Label(window, text="Title")
+lbl_author = Label(window, text="Author")
+lbl_isbn = Label(window, text="ISBN")
+
 
 title_text = StringVar()
-entry_title = Entry(window, textvariable=title_text , bg="floral white")
+entry_title = Entry(window, textvariable=title_text, bg="floral white")
 
 author_text = StringVar()
 entry_author = Entry(window, textvariable=author_text, bg="floral white")
@@ -85,10 +89,8 @@ entry_author = Entry(window, textvariable=author_text, bg="floral white")
 isbn_text = StringVar()
 entry_isbn = Entry(window, textvariable=isbn_text, bg="floral white")
 
-lb1.grid(row=0,column=0)
-lb2.grid(row=1,column=2)
-lb3.grid(row=2,column=0)
-#lb4.grid(row=3,column=0)
+
+lb2.grid(row=1, column=2, pady=30, padx=50, columnspan=4)
 
 lbl_title.grid(row=4, column=1)
 entry_title.grid(row=4, column=2)
@@ -97,7 +99,6 @@ entry_isbn.grid(row=5, column=2)
 lbl_author.grid(row=6, column=1)
 entry_author.grid(row=6, column=2)
 
-lb5.grid(row=7,column=1)
 
 list_space = Listbox(window, height=25, width=65, bg="floral white")
 list_space.grid(row=8, column=1, rowspan=8, columnspan=2)
@@ -110,22 +111,28 @@ scrollbar.configure(command=list_space.yview)
 
 list_space.bind('<<ListboxSelect>>', get_selected_row)
 
-btn_view = Button(window, text="View all", width=12, command=view_command, bg="lavenderBlush2")
-btn_view.grid(row=9, column=4)
+btn_view = Button(window, text="View all", width=12,
+                  command=view_command, bg="lavenderBlush2")
+btn_view.grid(row=9, column=4, padx=0)
 
-btn_search = Button(window, text="Search entry",width=12, command=search_command, bg="lavenderBlush2")
-btn_search.grid(row=10, column=4)
+btn_search = Button(window, text="Search entry", width=12,
+                    command=search_command, bg="lavenderBlush2")
+btn_search.grid(row=10, column=4, padx=0)
 
-btn_add = Button(window, text="Add entry", width=12, command=add_command, bg="lavenderBlush2")
-btn_add.grid(row=11, column=4)
+btn_add = Button(window, text="Add entry", width=12,
+                 command=add_command, bg="lavenderBlush2")
+btn_add.grid(row=11, column=4, padx=0)
 
-btn_update = Button(window, text="Update selected",width=12, command=update_command,bg="lavenderBlush2")
-btn_update.grid(row=12, column=4)
+btn_update = Button(window, text="Update selected", width=12,
+                    command=update_command, bg="lavenderBlush2")
+btn_update.grid(row=12, column=4, padx=0)
 
-btn_delete = Button(window, text="Delete selected", width=12, command=delete_command, bg="lavenderBlush2")
-btn_delete.grid(row=13, column=4)
+btn_delete = Button(window, text="Delete selected", width=12,
+                    command=delete_command, bg="lavenderBlush2")
+btn_delete.grid(row=13, column=4, padx=0)
 
-btn_close = Button(window, text="Close", width=12, command=window.destroy, bg="lavenderBlush2")
+btn_close = Button(window, text="Close", width=12,
+                   command=window.destroy, bg="lavenderBlush2")
 btn_close.grid(row=14, column=4)
-
+window.wm_attributes('-transparentcolor', 'green')
 window.mainloop()
